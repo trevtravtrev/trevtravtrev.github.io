e0ec31c7d2e696bc0cd915aa87b28f92a84e54c8
Trevor White <trevor.white@wayne.edu>
2023-09-25 18:36:58 -0400

    Update GitProfile.jsx

--- src/components/GitProfile.jsx
+++ src/components/GitProfile.jsx
@@ -157,14 +157,6 @@ const GitProfile = ({ config }) => {
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 rounded-box">
                   <div className="col-span-1">
                     <div className="grid grid-cols-1 gap-6">
-                      {!sanitizedConfig.themeConfig.disableSwitch && (
-                        <ThemeChanger
-                          theme={theme}
-                          setTheme={setTheme}
-                          loading={loading}
-                          themeConfig={sanitizedConfig.themeConfig}
-                        />
-                      )}
                       <AvatarCard
                         profile={profile}
                         loading={loading}
@@ -222,6 +214,14 @@ const GitProfile = ({ config }) => {
               >
                 <div className="card compact bg-base-100 shadow">
                   <Footer content={sanitizedConfig.footer} loading={loading} />
+                  {!sanitizedConfig.themeConfig.disableSwitch && (
+                        <ThemeChanger
+                          theme={theme}
+                          setTheme={setTheme}
+                          loading={loading}
+                          themeConfig={sanitizedConfig.themeConfig}
+                        />
+                      )}
                 </div>
               </footer>
             </Fragment>
