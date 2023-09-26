a34e75fc184fc81a87330775631acb2fc439d98c
Trevor White <trevor.white@wayne.edu>
2023-09-26 10:18:49 -0400

    Theme dropdown open upward.

--- src/components/theme-changer/index.jsx
+++ src/components/theme-changer/index.jsx
@@ -1,11 +1,10 @@
 import { AiOutlineControl } from 'react-icons/ai';
 import { skeleton } from '../../helpers/utils';
 import PropTypes from 'prop-types';
-import { useState, useRef } from 'react'; // Import useRef
+import { useState } from 'react';
 
 const ThemeChanger = ({ theme, setTheme, loading, themeConfig }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef(null); // Create a ref for the dropdown content
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -20,30 +19,11 @@ const ThemeChanger = ({ theme, setTheme, loading, themeConfig }) => {
 
     setTheme(selectedTheme);
 
-    setIsOpen(false); // Close the dropdown after selecting a theme
+    setIsOpen(false);
   };
-
-  const calculateDropdownPosition = () => {
-    if (dropdownRef.current) {
-      const dropdownHeight = dropdownRef.current.clientHeight;
-      const windowHeight = window.innerHeight;
-      const buttonRect = dropdownRef.current.getBoundingClientRect();
-
-      // Calculate the top position of the dropdown content
-      let top = buttonRect.bottom + dropdownHeight > windowHeight
-        ? -dropdownHeight
-        : buttonRect.height;
-
-      return { top };
-    }
-
-    return { top: 0 };
-  };
-
-  const dropdownPosition = calculateDropdownPosition();
 
   return (
-    <div className="card overflow-visible shadow-lg compact bg-base-100">
+    <div className="card overflow-visible shadow-lg compact bg-base-100 relative">
       <div className="flex-row items-center space-x-4 flex pl-6 pr-2 py-4">
         <div className="flex-1">
           <h5 className="card-title">
@@ -73,7 +53,7 @@ const ThemeChanger = ({ theme, setTheme, loading, themeConfig }) => {
               <div
                 tabIndex={0}
                 className="btn btn-ghost m-1 normal-case opacity-50 text-base-content"
-                onClick={toggleDropdown} // Open/close dropdown when clicking the button
+                onClick={toggleDropdown}
               >
                 <AiOutlineControl className="inline-block w-5 h-5 stroke-current md:mr-2" />
                 <span className="hidden md:inline">Change Theme</span>
@@ -82,40 +62,36 @@ const ThemeChanger = ({ theme, setTheme, loading, themeConfig }) => {
                   viewBox="0 0 1792 1792"
                   className="inline-block w-4 h-4 ml-1 fill-current"
                 >
-                  <path d="M1395 736q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23z" />
+                  <path d="M1395 736q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10-10 10-23z" />
                 </svg>
               </div>
-              {isOpen && (
-                <div
-                  tabIndex={0}
-                  className="mt-16 overflow-y-auto shadow-2xl top-px dropdown-content max-h-96 w-52 rounded-lg bg-base-200 text-base-content z-10"
-                  style={{ top: dropdownPosition.top }} // Apply calculated top position
-                  ref={dropdownRef} // Assign the ref to the dropdown content
-                >
-                  <ul className="p-4 menu compact">
-                    {[
-                      themeConfig.defaultTheme,
-                      ...themeConfig.themes.filter(
-                        (item) => item !== themeConfig.defaultTheme
-                      ),
-                    ].map((item, index) => (
-                      <li key={index}>
-                        {/* eslint-disable-next-line */}
-                        <a
-                          onClick={(e) => changeTheme(e, item)}
-                          className={`${theme === item ? 'active' : ''}`}
-                        >
-                          <span className="opacity-60 capitalize">
-                            {item === themeConfig.defaultTheme
-                              ? 'Default'
-                              : item}
-                          </span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
+              <div
+                className={`${
+                  isOpen ? 'block' : 'hidden'
+                } absolute right-0 mt-8 w-52 rounded-lg bg-base-200 text-base-content z-10`}
+              >
+                <ul className="p-4 menu compact">
+                  {[
+                    themeConfig.defaultTheme,
+                    ...themeConfig.themes.filter(
+                      (item) => item !== themeConfig.defaultTheme
+                    ),
+                  ].map((item, index) => (
+                    <li key={index}>
+                      <a
+                        onClick={(e) => changeTheme(e, item)}
+                        className={`${theme === item ? 'active' : ''}`}
+                      >
+                        <span className="opacity-60 capitalize">
+                          {item === themeConfig.defaultTheme
+                            ? 'Default'
+                            : item}
+                        </span>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           )}
         </div>
